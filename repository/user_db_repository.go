72b836ada9743362516928647ac12fb34a306889
aleksma <aleksma99@yandex.ru>
2019-10-16 10:17:47 +0300

    [ADD] users DB methods

--- repository/user_db_repository.go
+++ repository/user_db_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/go-park-mail-ru/2019_2_CoolCode/models"
 )
 
@@ -10,15 +11,29 @@ type DBUserStore struct {
 }
 
 func (userStore *DBUserStore) GetUserByEmail(email string) (models.User, error) {
-	panic("implement me")
+	user := &models.User{}
+	selectStr := "SELECT id, username, email, name, password, status, phone FROM users WHERE email = $1"
+	row := userStore.DB.QueryRow(selectStr, email)
+
+	row.Scan(&user.ID, &user.Username, &user.Email, &user.Name, &user.Password, &user.Status, &user.Phone)
+	return *user, nil
 }
 
 func (userStore *DBUserStore) GetUserByID(ID uint64) (models.User, error) {
-	panic("implement me")
+	user := &models.User{}
+	selectStr := "SELECT id, username, email, name, password, status, phone FROM users WHERE id = $1"
+	row := userStore.DB.QueryRow(selectStr, ID)
+
+	row.Scan(&user.ID, &user.Username, &user.Email, &user.Name, &user.Password, &user.Status, &user.Phone)
+	return *user, nil
 }
 
 func (userStore *DBUserStore) PutUser(newUser *models.User) error {
-	panic("implement me")
+	result, _ := userStore.DB.Exec("INSERT INTO users (username, email, name, password, status, phone) VALUES ($1, $2, $3, $4, $5, $6)",
+		newUser.Username, newUser.Email, newUser.Name, newUser.Password, newUser.Status, newUser.Phone)
+	affected, _ := result.RowsAffected()
+	fmt.Printf("Rows affected: %v", affected)
+	return nil
 }
 
 func (userStore *DBUserStore) Replace(ID uint64, newUser *models.User) error {
@@ -26,13 +41,38 @@ func (userStore *DBUserStore) Replace(ID uint64, newUser *models.User) error {
 }
 
 func (userStore *DBUserStore) Contains(user models.User) bool {
-	panic("implement me")
+	sourceUser := &models.User{}
+	selectStr := "SELECT id, username, email, name, password, status, phone FROM users WHERE email = $1"
+	row := userStore.DB.QueryRow(selectStr, user.Email)
+
+	err := row.Scan(&sourceUser.ID, &sourceUser.Username, &sourceUser.Email, &sourceUser.Name, &sourceUser.Password, &sourceUser.Status, &sourceUser.Phone)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func (userStore *DBUserStore) GetUsers() models.Users {
-	panic("implement me")
+	userSlice := models.Users{}
+
+	rows, _ := userStore.DB.Query("SELECT id, username, email, name, password, status, phone FROM users")
+	for rows.Next() {
+		user := &models.User{}
+		rows.Scan(&user.ID, &user.Username, &user.Email, &user.Name, &user.Password, &user.Status, &user.Phone)
+		userSlice.Users = append(userSlice.Users, user)
+	}
+	rows.Close()
+
+	return userSlice
 }
 
+// TODO: handle errors
 func NewUserDBStore() UserRepo {
-	return &DBUserStore{}
+	connStr := "user=root password=1234 dbname=coolslackdb sslmode=disable"
+	db, _ := sql.Open("postgres", connStr)
+	db.Ping()
+
+	return &DBUserStore{
+		db,
+	}
 }
