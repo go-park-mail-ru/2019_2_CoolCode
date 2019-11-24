e697b5155da7c7595ce90f025b871429e7a2e8b9
Anton <apriyma0@gmail.com>
2019-11-24 20:49:13 +0300

    [ADD]Mutex

--- repository/notification_array_repository.go
+++ repository/notification_array_repository.go
@@ -1,19 +1,29 @@
 package repository
 
-import "github.com/go-park-mail-ru/2019_2_CoolCode/models"
+import (
+	"github.com/go-park-mail-ru/2019_2_CoolCode/models"
+	"sync"
+)
 
 type NotificationArrayRepository struct {
-	Hubs map[uint64]*models.WebSocketHub
+	Hubs  map[uint64]*models.WebSocketHub
+	mutex sync.Mutex
 }
 
 func (n *NotificationArrayRepository) GetNotificationHub(chatID uint64) *models.WebSocketHub {
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
 	if hub, ok := n.Hubs[chatID]; ok {
 		return hub
 	}
 	n.Hubs[chatID] = models.NewHub()
 	return n.Hubs[chatID]
+
 }
 
 func NewArrayRepo() NotificationRepository {
-	return &NotificationArrayRepository{Hubs: make(map[uint64]*models.WebSocketHub, 0)}
+	return &NotificationArrayRepository{
+		Hubs:  make(map[uint64]*models.WebSocketHub, 0),
+		mutex: sync.Mutex{},
+	}
 }
